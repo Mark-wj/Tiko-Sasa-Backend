018a8f17e2f2dba79e2529c34dea5efc53239ce5
Mark Njenga <marknjenga25@gmail.com>
2025-03-11 10:07:53 +0300

    Change the database connection

--- Accounts/settings.py
+++ Accounts/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'Accounts.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost/tikosasa')
+    'default': dj_database_url.config(default='postgres://username:password@hostname:port/tikosasa')
 }
 
 
