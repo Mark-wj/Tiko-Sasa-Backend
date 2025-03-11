871c0ede6672ae62833d9058dd7ec73beae188c7
Mark Njenga <marknjenga25@gmail.com>
2025-03-11 11:04:27 +0300

    Change the database connection

--- Accounts/settings.py
+++ Accounts/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'Accounts.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_PUBLIC_URL'))
 }
 
 
